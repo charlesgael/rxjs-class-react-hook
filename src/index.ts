c4f4f4c40684697e0e4a0a82ee7a83ac62bab215
Charles Gael DJEDAI <charlesgael.djedai@outlook.com>
2020-04-07 00:09:43 +0200

    v1.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as Selector } from "./Selector";
 export { default as Store } from "./Store";
+export { default as PersistStore } from "./PersistStore";
+export * from "./Persistor";
